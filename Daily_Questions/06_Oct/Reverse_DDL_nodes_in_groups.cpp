1acbea2c702453a98240c8f6ece5df38ca9a9736
mairohanhoon <rohantiwari2709@gmail.com>
2023-10-06 21:36:14 +0530

    DAILY PROBLEM: added daily problem of 6th Oct

---
+++ Daily_Questions/06_Oct/Reverse_DDL_nodes_in_groups.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h> 
+
+/*********************************************************
+
+    Following is the class structure of the Node class:
+
+    class Node
+	{
+	public:
+	    int data;
+	    Node *next;
+	    Node *prev;
+	    Node(int data)
+	    {
+	        this->data = data;
+	        this->next = NULL;
+	        this->prev = NULL;
+	    }
+	};
+
+************************************************************/
+
+Node* reverseDLLInGroups(Node* head, int k)
+{	
+    // Write your code here. 
+	vector<int> listt;
+	vector<int> res;
+	Node* ptr = head;
+	while(ptr!=NULL){
+		listt.push_back(ptr->data);
+		ptr = ptr->next;
+	}
+	int counter = 0;
+	int end = k;
+	int n = listt.size();
+	while(counter < listt.size()){
+		vector<int> temp;
+		for(int i=counter; i<min(n, end); i++){
+			temp.push_back(listt[i]);
+		}
+		reverse(temp.begin(), temp.end());
+		res.insert(res.end(), temp.begin(), temp.end());
+		counter+=k;
+		end+=k;
+	}
+	ptr = head;
+	for(int i=0; i<res.size(); i++){
+		ptr->data = res[i];
+		ptr = ptr->next;
+	}
+	return head;
+}
